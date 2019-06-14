1000b64c239679c564771fa99244dd92475f2e0f
Luca Guindani <luca.guindani@unil.ch>
2019-06-14 10:43:39 +0200

    Add status CRUD

---
+++ site/database/migrations/2019_06_14_101546_create_status_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('status', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('status');
+    }
+}
--- site/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ site/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,2 +1,3 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
+<li><a href='{{ backpack_url('status') }}'><i class='fa fa-tasks'></i> <span>Statuts</span></a></li>
