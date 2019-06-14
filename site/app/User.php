0c4e21523c782b2414333a0bb86d1993acf15734
Luca Guindani <luca.guindani@unil.ch>
2019-06-14 11:38:40 +0200

    Add users, roles & permissions

--- site/app/User.php
+++ site/app/User.php
@@ -5,10 +5,14 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Backpack\CRUD\CrudTrait;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use CrudTrait;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
--- site/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ site/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,3 +2,13 @@
 <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
 <li><a href='{{ backpack_url('status') }}'><i class='fa fa-tasks'></i> <span>Statuts</span></a></li>
 <li><a href='{{ backpack_url('type') }}'><i class='fa fa-th'></i> <span>Types</span></a></li>
+
+<!-- Users, Roles Permissions -->
+<li class="treeview">
+    <a href="#"><i class="fa fa-group"></i> <span>Utilisateurs</span> <i class="fa fa-angle-left pull-right"></i></a>
+    <ul class="treeview-menu">
+        <li><a href="{{ backpack_url('user') }}"><i class="fa fa-user"></i> <span>Utilisateurs</span></a></li>
+        <li><a href="{{ backpack_url('role') }}"><i class="fa fa-group"></i> <span>Rôles</span></a></li>
+        <li><a href="{{ backpack_url('permission') }}"><i class="fa fa-key"></i> <span>Permissions</span></a></li>
+    </ul>
+</li>
