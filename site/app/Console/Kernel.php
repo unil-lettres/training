a1eb9f1dc0a209cbdfe456ebd361e6aac275ef50
Luca Guindani <luca.guindani@unil.ch>
2019-06-19 16:08:44 +0200

    Added telescope prune schedule

--- site/app/Console/Kernel.php
+++ site/app/Console/Kernel.php
@@ -24,8 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        if ($this->app->isLocal()) {
+            $schedule->command('telescope:prune')->daily();
+        }
     }
 
     /**
