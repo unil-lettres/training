c21c845893eaf03943bd5928607d93bd6979f1c3
Luca Guindani <luca.guindani@gmail.com>
2020-07-01 15:31:32 +0200

    Fix duplicated route name

--- site/routes/web.php
+++ site/routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', 'HomeController@index')->name('home');
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index');
 
 Route::get('/contact', function () {
     return view('pages.contact');
