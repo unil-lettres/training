a73c6b5fd14e5293b24cda174e7f0c08af85f550
Luca Guindani <luca.guindani@gmail.com>
2022-04-29 11:05:42 +0200

    Add maintenance mode custom view

---
+++ site/resources/views/maintenance.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>formalett.unil.ch</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+        .full-height {
+            height: 100vh;
+        }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+        .position-ref {
+            position: relative;
+        }
+        .content {
+            text-align: center;
+        }
+        .title {
+            font-size: 84px;
+        }
+        .subtitle {
+            font-size: 30px;
+        }
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            formalett.unil.ch
+        </div>
+        <div class="subtitle">
+            L'application est temporairement indisponible en raison d'une mise à jour planifiée. Revenez plus tard, s'il vous plaît.
+        </div>
+    </div>
+</div>
+</body>
+</html>
