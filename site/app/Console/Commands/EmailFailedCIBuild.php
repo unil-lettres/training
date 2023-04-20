5ef423eb2be59922c5bf6c2ec0852e2fd7137b97
Luca Guindani <luca.guindani@gmail.com>
2023-04-20 15:35:50 +0200

    Fix email:failure laravel command

--- site/app/Console/Commands/EmailFailedCIBuild.php
+++ site/app/Console/Commands/EmailFailedCIBuild.php
@@ -34,12 +34,10 @@ class EmailFailedCIBuild extends Command
 
     /**
      * Execute the console command.
-     *
-     * @return mixed
      */
-    public function handle()
+    public function handle(): void
     {
-        return Mail::to('mailtrap@test.com')
+        Mail::to('mailtrap@test.com')
             ->send(new DuskFailure());
     }
 }
