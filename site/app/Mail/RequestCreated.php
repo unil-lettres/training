53dffa7a71601be74b2270cbb3df862b5d65df71
Luca Guindani <luca.guindani@gmail.com>
2024-01-22 15:39:59 +0100

    Add header for tags Fix multi artifact upload in ci

--- site/app/Mail/RequestCreated.php
+++ site/app/Mail/RequestCreated.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use App\Models\Request;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Headers;
 use Illuminate\Queue\SerializesModels;
 
 class RequestCreated extends Mailable
@@ -26,6 +27,18 @@ class RequestCreated extends Mailable
     public function __construct(Request $request)
     {
         $this->request = $request;
+    }
+
+    /**
+     * Get the message headers.
+     */
+    public function headers(): Headers
+    {
+        return new Headers(
+            text: [
+                'X-Tags' => 'Formalett',
+            ],
+        );
     }
 
     /**
