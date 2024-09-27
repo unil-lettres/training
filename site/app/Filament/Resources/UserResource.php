5b1bedc9cc2b4932c5ba809fd0c61fc1515f3852
Luca Guindani <luca.guindani@gmail.com>
2024-09-27 09:32:18 +0200

    Add users & roles

---
+++ site/app/Filament/Resources/UserResource.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\Pages\CreateUser;
+use App\Filament\Resources\UserResource\Pages\EditUser;
+use App\Filament\Resources\UserResource\Pages\ListUsers;
+use App\Filament\Resources\UserResource\Pages\ViewUser;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\User;
+use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Split;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    protected static ?string $modelLabel = 'utilisateur';
+
+    protected static ?string $navigationGroup = 'Paramètres';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('Nom')
+                            ->required()
+                            ->maxLength(191)
+                            ->columnSpanFull(),
+                        TextInput::make('email')
+                            ->label('Email')
+                            ->email()
+                            ->required()
+                            ->maxLength(191)
+                            ->columnSpanFull(),
+                        TextInput::make('password')
+                            ->label('Mot de passe')
+                            ->password()
+                            ->revealable()
+                            ->rules([
+                                Password::min(8)
+                                    ->letters()
+                                    ->numbers()
+                            ])
+                            ->columnSpanFull(),
+                    ]),
+
+                Section::make()
+                    ->schema([
+                        Select::make('roles')
+                            ->label('Rôles')
+                            ->multiple()
+                            ->searchable()
+                            ->options(User::$role),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Nom')
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->label('Email')
+                    ->searchable()
+                    ->icon('heroicon-m-envelope'),
+                TextColumn::make('roles')
+                    ->label('Rôles')
+                    ->badge(),
+                TextColumn::make('created_at')
+                    ->label('Date de création')
+                    ->dateTime('j M Y, H:i')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->label('Date de modification')
+                    ->dateTime('j M Y, H:i')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                SelectFilter::make('roles')
+                    ->label('Rôles')
+                    ->options(User::$role)
+                    ->query(function (Builder $query, array $data): Builder {
+                        if (!empty($data['value'])) {
+                            $query->whereJsonContains('roles', $data['value']);
+                        }
+                        return $query;
+                    }),
+            ])
+            ->actions([
+                ViewAction::make()
+                    ->label(''),
+                EditAction::make()
+                    ->label(''),
+                DeleteAction::make()
+                    ->label(''),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListUsers::route('/'),
+            'create' => CreateUser::route('/create'),
+            'view' => ViewUser::route('/{record}'),
+            'edit' => EditUser::route('/{record}/edit'),
+        ];
+    }
+}
--- site/app/User.php
+++ site/app/User.php
@@ -7,6 +7,7 @@ use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
@@ -21,7 +22,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'roles',
     ];
 
     /**
@@ -33,10 +34,16 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    public static $role = [
+        'admin' => 'Admin',
+        'notification' => 'Notification',
+    ];
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
+            'roles' => 'array',
         ];
     }
 
@@ -46,5 +53,57 @@ class User extends Authenticatable
     public function requests(): HasMany
     {
         return $this->hasMany(Request::class);
+    }
+
+    // Set the user's password
+    public function setPasswordAttribute(?string $value): void
+    {
+        if (!empty($value)) {
+            $this->attributes['password'] = Hash::make($value);
+        }
+    }
+
+    /**
+     * Check if the user has a certain role
+     */
+    public function hasRole(string|array $roles): bool
+    {
+        if (is_array($roles)) {
+            return !empty(array_intersect($roles, $this->roles ?? []));
+        }
+
+        return in_array($roles, $this->roles ?? []);
+    }
+
+    /**
+     * Assign roles to the user
+     */
+    public function assignRoles(array|string $roles): void
+    {
+        // Merge new roles with existing ones
+        $existingRoles = $this->roles ?? [];
+
+        // Convert roles to array if it's a string
+        if (is_string($roles)) {
+            $roles = explode(',', $roles);
+        }
+
+        $this->roles = array_unique(array_merge($existingRoles, $roles));
+        $this->save();
+    }
+
+    /**
+     * Remove roles from the user
+     */
+    public function removeRoles(array|string $roles): void
+    {
+        $existingRoles = $this->roles ?? [];
+
+        if (is_string($roles)) {
+            $roles = explode(',', $roles);
+        }
+
+        $this->roles = array_diff($existingRoles, $roles);
+        $this->save();
     }
 }
