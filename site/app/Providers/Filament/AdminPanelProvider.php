5c60194b8ef2893f20caf7a513db1e4c8353f995
Luca Guindani <luca.guindani@gmail.com>
2024-10-01 13:16:22 +0200

    Add spa but exclude homepage

--- site/app/Providers/Filament/AdminPanelProvider.php
+++ site/app/Providers/Filament/AdminPanelProvider.php
@@ -63,6 +63,10 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->spa()
+            ->spaUrlExceptions(fn (): array => [
+                url('/'),
             ]);
     }
 }
