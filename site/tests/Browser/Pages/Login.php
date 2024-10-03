e182893671992ec2db492dc5fc1d73adc2901960
Luca Guindani <luca.guindani@gmail.com>
2024-10-03 14:15:55 +0200

    Test 1

--- site/tests/Browser/Pages/Login.php
+++ site/tests/Browser/Pages/Login.php
@@ -36,7 +36,7 @@ class Login extends Page
     {
         $browser->type('input[type=email]', $login)
             ->type('input[type=password]', $password)
-            //->pause(2000) // Avoid issues on GitHub Actions
+            //->pause(2000) // Avoid issues on GitHub Actions / Test 1
             ->click('button[type=submit]'); // Connexion button
     }
 }
