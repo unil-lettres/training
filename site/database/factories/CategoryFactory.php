4421103551bbf27ed8004982788856d36c0242e4
Luca Guindani <luca.guindani@gmail.com>
2024-10-04 13:49:29 +0200

    Use randomNumber to avoid duplicates in factories

--- site/database/factories/CategoryFactory.php
+++ site/database/factories/CategoryFactory.php
@@ -12,7 +12,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->unique()->word(),
+            'name' => $this->faker->word() . ' ' . $this->faker->randomNumber(5, false),
         ];
     }
 }
