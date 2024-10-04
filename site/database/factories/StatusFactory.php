1f84958ad6cac3757aaa613e7633759c20401029
Luca Guindani <luca.guindani@gmail.com>
2024-10-04 13:51:24 +0200

    Code style

--- site/database/factories/StatusFactory.php
+++ site/database/factories/StatusFactory.php
@@ -12,7 +12,7 @@ class StatusFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->word() . ' ' . $this->faker->randomNumber(5, false),
+            'name' => $this->faker->word().' '.$this->faker->randomNumber(5, false),
         ];
     }
 }
