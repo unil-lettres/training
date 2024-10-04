ccd374d9fb7e465867e30a3bc6272ca669354ed0
Luca Guindani <luca.guindani@gmail.com>
2024-10-04 13:42:54 +0200

    Add unique to faker->word in factories (avoid duplicates)

--- site/database/factories/RequestFactory.php
+++ site/database/factories/RequestFactory.php
@@ -23,12 +23,8 @@ class RequestFactory extends Factory
             'theme' => $this->faker->sentence(3),
             'deadline' => now()->addMonth(),
             'extras' => json_decode('{"doctoral_school":null,"fns":"1","doctoral_status":null,"doctoral_level":null,"tested_products":null,"teachers_nbr":"0","students_nbr":"0","action_type":"0"}', true),
-            'status_admin' => $this->faker->randomElement(
-                Request::$status
-            ),
-            'type' => $this->faker->randomElement(
-                Request::$type
-            ),
+            'status_admin' => $this->faker->randomElement(Request::$status),
+            'type' => $this->faker->randomElement(Request::$type),
             'category_id' => Category::factory(),
             'status_id' => Status::factory(),
             'user_id' => User::factory(),
