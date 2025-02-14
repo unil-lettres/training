014c0f7139aed09f6a20e9cdac9cd1f0dc633ce7
Luca Guindani <luca.guindani@gmail.com>
2025-02-14 16:55:08 +0100

    Increase databaseNotificationsPolling

--- site/app/Providers/Filament/AdminPanelProvider.php
+++ site/app/Providers/Filament/AdminPanelProvider.php
@@ -34,7 +34,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->databaseNotifications()
-            ->databaseNotificationsPolling('15s')
+            ->databaseNotificationsPolling('30s')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
